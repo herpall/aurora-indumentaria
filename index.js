c8f86b95e75210daf1978ce93dc326c0fac88e8b
herpall <hernan.palleiro@hotmail.com>
2022-03-24 13:13:07 -0300

    carritoo

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const clickButton = document.querySelectorAll('.button')
-const tbody = document.querySelector('.tbody')
-let carrito = []
+let carrito = JSON.parse(localStorage.getItem('carrito'))
 
+if (carrito == null) {
+    carrito = [];
+}
+
+const tbody = document.querySelector('.tbody')
+
+const clickButton = document.querySelectorAll('.button')
 clickButton.forEach(btn => {
     btn.addEventListener('click', addToCarritoItem)
 })
 
-// Con closest se obtiene la clase mas cercana que sea 'card' que es la clase madre de la tarjeta
+renderCarrito();
+
 function addToCarritoItem(e) {
+    e.preventDefault()
+
     const button = e.target
-    const item = button.closest('.card') 
-    const itemTitle = item.querySelector('.card-title').textContent;
-    const itemPrice = item.querySelector('.card-price').textContent;
-    const itemImg = item.querySelector('.card-img-top').src;
+    const item = button.closest('.card')
+    const itemTitle = item.querySelector('.card-title').textContent
+    const itemPrice = item.querySelector('.card-price').textContent
+    const itemImg = item.querySelector('.card-img-top').src
 
     const newItem = {
         title: itemTitle,
@@ -20,42 +28,62 @@ function addToCarritoItem(e) {
         img: itemImg,
         cantidad: 1,
     }
-
+    
     addItemCarrito(newItem)
 }
 
-// Le agrego al array de carrito el newItem
 function addItemCarrito(newItem) {
+
+    const inputElemento = document.getElementsByClassName('input__elemento');
+
+    for(let i = 0; i < carrito.length; i++) {
+        if (carrito[i].title.trim() === newItem.title.trim()) {
+            carrito[i].cantidad++;
+            const inputValue = inputElemento[i]
+            inputValue.value++;
+            console.log(carrito)
+            return null
+        }
+    }   
+
     carrito.push(newItem)
+
+    // GUardas el carrito en localStorage
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+
     renderCarrito()
 }
 
+// Cuando voy a iniciar a renderizarlo que este vacio el tbody por eso las comillas vacias
+// Creamos el elemento tr y luego le colocamos una clase
 function renderCarrito() {
-    // Cada vez que se ejecute el tbody, tiene que estar vacio
+    if (tbody == null) {
+        return
+    }
+
     tbody.innerHTML = ''
-    // Vamos a hacer el cambio con el map dentro de carrito
+
     carrito.map(item => {
-        // Creamos a tr que es la nueva fila (nuevo elemento o producto)
         const tr = document.createElement('tr')
-        // Agregamos una clase al tr
         tr.classList.add('itemCarrito')
-        // Agregaremos un contenido dentro del tr
-        const Content = `
+
+        const content = `
+        
             <th scope="row">1</th>
             <td class="table__productos">
                 <img src=${item.img} alt="">
                 <h6 class="tittle">${item.title}</h6>
             </td>
-            <td class="table__precios"><p>${item.price}</p></td>
+            <td class="table__precios"><p>${item.precio}</p></td>
             <td class="table__cantidad">
-                <input type="number" min="1" value=${item.cantidad}>
+                <input type="number" min="1" value=${item.cantidad} class="input__elemento">
                 <button class="delete btn btn-danger">x</button>
             </td>
+        
         `
 
-        // Aca decimos que al tr le agregamos content
-        tr.innerHTML = Content
-        // a tbody le agregamos el hijo tr
+        tr.innerHTML = content
+        
         tbody.append(tr)
     })
 }
